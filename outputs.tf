5fe124cd3a088daa725e1a4919c50098f07f8d03
Michael Datz <michael.kennedydatz@hashicorp.com>
2026-05-29 10:32:44 -0400

    remove potentially sensitive output

--- outputs.tf
+++ outputs.tf
@@ -1,8 +1,3 @@
-output "github_oauth_token_id" {
-  description = "The ID of the GitHub OAuth token used for VCS integration."
-  value = var.github_oauth_token_id
-}
-
 output "hcp_terraform_management_workspace_id" {
   description = "The ID of the Terraform Cloud workspace for example-app-management."
   value = tfe_workspace.this.id
